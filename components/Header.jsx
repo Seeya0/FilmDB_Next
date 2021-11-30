fc4fea7fd6c26094c35cd7a84b4a3d403c3fe2ba
yoshioka.seiya <yoshioka.seiya@hc-sys.jp>
2021-11-30 17:58:08 +0900

    Add Header and HeaderItems components

---
+++ components/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import HeaderItem from "./HeaderItem";
+import {
+  HomeIcon,
+  BadgeCheckIcon,
+  CollectionIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+
+const Header = () => {
+  return (
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="text-white text-5xl">Film DB</div>
+
+      <div className="flex max-w-2xl">
+        <HeaderItem title="Home" Icon={HomeIcon} />
+        <HeaderItem title="Trending" Icon={LightningBoltIcon} />
+        <HeaderItem title="Verified" Icon={BadgeCheckIcon} />
+        <HeaderItem title="Collections" Icon={CollectionIcon} />
+        <HeaderItem title="Search" Icon={SearchIcon} />
+        <HeaderItem title="Account" Icon={UserIcon} />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
