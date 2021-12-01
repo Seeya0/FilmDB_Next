0e7500b4aa3188154bdec66c3c0571f57cd4c94b
yoshioka.seiya <yoshioka.seiya@hc-sys.jp>
2021-12-01 16:51:20 +0900

    Modified Header and config

--- components/Header.jsx
+++ components/Header.jsx
@@ -12,9 +12,9 @@ import {
 const Header = () => {
   return (
     <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
-      <div className="text-white text-5xl">Film DB</div>
+      <div className="text-white text-6xl font-serif">FilmDB</div>
 
-      <div className="flex max-w-2xl">
+      <div className="flex flex-grow max-w-2xl justify-evenly ">
         <HeaderItem title="Home" Icon={HomeIcon} />
         <HeaderItem title="Trending" Icon={LightningBoltIcon} />
         <HeaderItem title="Verified" Icon={BadgeCheckIcon} />
